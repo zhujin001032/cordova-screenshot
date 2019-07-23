bec75608e864a034ff86516aa292731b24ea6eac
Jason He <zhujin001xb@163.com>
2019-07-23 15:19:26 +0800

    Update Screenshot.js

--- www/Screenshot.js
+++ www/Screenshot.js
@@ -21,7 +21,14 @@ module.exports = {
 			callback && callback(error);
 		}, "Screenshot", "saveScreenshot", [format, quality, filename]);
 	},
-
+	saveToAlbum:function(callback) {
+		
+		exec(function(res){
+			callback && callback(null,res);
+		}, function(error){
+			callback && callback(error);
+		}, "Screenshot", "saveScreenshotToAlbum", []);
+	},
 	URI:function(callback, quality){
 		quality = typeof(quality) !== 'number'?100:quality;
 		exec(function(res){
